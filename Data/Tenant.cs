debf27854c6b69059d7db9cba54099737d9a47f3
Arie Schwartzman <ariesch@microsoft.com>
2020-01-14 16:25:19 +0200

    show cards

--- Data/Tenant.cs
+++ Data/Tenant.cs
@@ -4,8 +4,15 @@ namespace Husyoudaddy.Data
 {
     public class Tenant : TableEntity
     {
-        public string name {get;set;}
-        public string email {get;set;}        
+        public string name { get; set; }
+        public string email { get; set; }
         public string friendly_name { get; set; }
+        public string saasSubscriptionId { get; set; }
+        public int maxMessages { get; set; }
+        public int msgCount { get; set; }
+        public string app_id {get;set;}
+        public string app_secret {get;set;}
+        public string botName { get; set; }
+        public string webchat_secret { get; set; }
     }
 }
