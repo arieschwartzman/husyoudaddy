7b28ab870eb2927a53ef3727eb5be0de7f563295
Arie <arie@schwartzman.info>
2020-01-18 17:56:31 +0200

    add selection of scenarios

--- Data/Scenario.cs
+++ Data/Scenario.cs
@@ -6,5 +6,9 @@ namespace Husyoudaddy.Data
   public class Scenario : TableEntity
   {
     public string name { get; set; }
+    public string scenario_trigger { get; set; }
+    public DateTime updated { get; set; }
+    public string description { get; set; }
+
   }
 }
