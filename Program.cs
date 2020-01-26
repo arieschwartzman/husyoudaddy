e55c7d243ecc0ae58c0c04535a0ca39c6907739a
arieschwartzman <ariesch@microsoft.com>
2020-01-26 19:55:32 +0200

    use managed identity for kv

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 
 namespace Husyoudaddy
@@ -19,11 +22,19 @@ namespace Husyoudaddy
         public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((ctx, config) =>
         {
-            var root = config.Build();
-            config.AddAzureKeyVault(
+          var root = config.Build();
+          var tokenProvider = new AzureServiceTokenProvider();
+          var kvClient = new KeyVaultClient((authority, resource, scope) => tokenProvider.KeyVaultTokenCallback(authority, resource, scope));
+          config.AddAzureKeyVault(root["KeyVault:VaultUrl"], kvClient, new DefaultKeyVaultSecretManager());
+
+         
+          //var root = config.Build();
+          /*
+              config.AddAzureKeyVault(
                    root["KeyVault:VaultUrl"],
                    root["KeyVault:ClientId"],
-                   root["ClientSecret"]);
+                   root["ClientSecret"]);*/
+                   
         })
         .ConfigureWebHostDefaults(webBuilder =>
         {
